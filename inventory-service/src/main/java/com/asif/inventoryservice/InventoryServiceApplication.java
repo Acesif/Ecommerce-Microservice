22035993fa32c59789438ca0b059361cbdd4a607
Asif <asifpalak@gmail.com>
2024-09-10 13:17:23 +0600

    refactored into a parent project

--- inventory-service/src/main/java/com/asif/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/asif/inventoryservice/InventoryServiceApplication.java
@@ -2,8 +2,10 @@ package com.asif.inventoryservice;
 
 import com.asif.inventoryservice.model.Inventory;
 import com.asif.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -12,17 +14,20 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
-    private void loadData(InventoryRepository inventoryRepository){
-        Inventory item1 = Inventory.builder()
-                .skuCode("amoogus")
-                .quantity(100)
-                .build();
-        inventoryRepository.save(item1);
-        Inventory item2 = Inventory.builder()
-                .skuCode("gyaat")
-                .quantity(109)
-                .build();
-        inventoryRepository.save(item2);
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory item1 = Inventory.builder()
+                    .skuCode("amoogus")
+                    .quantity(100)
+                    .build();
+            Inventory item2 = Inventory.builder()
+                    .skuCode("gyaat")
+                    .quantity(109)
+                    .build();
+            inventoryRepository.save(item1);
+            inventoryRepository.save(item2);
+        };
     }
 
 }
